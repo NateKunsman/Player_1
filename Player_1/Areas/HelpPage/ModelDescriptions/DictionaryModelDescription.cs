8bef55c26548d1dcdc08697b7440666989bd7ec3
NateKunsman <nkkuns@protonmail.com>
2021-10-21 13:41:38 -0400

    added repo

---
+++ Player_1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Player_1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Player_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Player_1.WebApiApplication" Language="C#" %>
