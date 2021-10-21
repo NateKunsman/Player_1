4e6e7ac0bcb1ab6276e1c244e2fc8e65a4fbd63a
NateKunsman <nkkuns@protonmail.com>
2021-10-21 16:52:51 -0400

    added Genre create and list classes as well as a GenreService

--- Player_1.Service/GenreService.cs
+++ Player_1.Service/GenreService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Player_1.Data;
+using Player_1.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,40 @@ using System.Threading.Tasks;
 
 namespace Player_1.Service
 {
-    class GenreService
+    public class GenreService
     {
+        private readonly Guid _UserId;
+
+        public GenreService(Guid userId)
+        {
+            userId = _UserId;
+        }
+        public bool GenreCreate(GenreCreate model)
+        {
+            var entity =
+                new Genre()
+                {
+                    UserId = _UserId,
+                    GenreName = model.GenreName,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Genres.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool DeleteGenre(int genreId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Genres
+                        .Single(e => e.GenreId == genreId) /*&& e.AuthorId == _genreId)*/;
+                ctx.Genres.Remove(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
     }
 }
