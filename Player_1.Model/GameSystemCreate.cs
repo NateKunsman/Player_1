d8381621875ae59d326e643c5ac4634d20f3deb0
CJ <thompsoncj93@gmail.com>
2021-10-22 09:20:15 -0400

    addedGameSystem

---
+++ Player_1.Model/GameSystemCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_1.Model
+{
+    public class GameSystemCreate
+    {
+        //Name of game system
+        [Required]
+        public string  GameSystemName { get; set; }
+    }
+}
---
+++ Player_1.Service/GameSystemService.cs
@@ -0,0 +1,34 @@
+﻿using Player_1.Data;
+using Player_1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_1.Service
+{
+    public class GameSystemService
+    {
+        private readonly Guid _UserId;
+
+        public GameSystemService(Guid userId)
+        {
+            _UserId = userId;
+        }
+    public bool CreateGameSystem(GameSystemCreate model)
+    {
+        var entity =
+            new GameSystem()
+            {
+                GameSystemName = model.GameSystemName,
+            };
+        using (var ctx = new ApplicationDbContext())
+        {
+            ctx.GameSystems.Add(entity);
+            return ctx.SaveChanges() == 1;
+        }
+    }
+
+  }
+}
